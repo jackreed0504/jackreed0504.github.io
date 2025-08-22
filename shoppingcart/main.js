bb59c4dcf27928ae8601f79bf8c59239bad5329a
jackreed0504 <s4007359@student.rmit.edu.au>
2025-08-22 12:19:15 +1000

    JavaScript practice

---
+++ shoppingcart/main.js
@@ -0,0 +1,30 @@
+const cartTotalText = document.querySelector("#cartTotal");
+let cartTotal = 0;
+
+let shoppingCart = [
+  { name: "t-shirt", price: 20 },
+  { name: "jeans", price: 50 },
+  { name: "sneakers", price: 80 },
+  { name: "backpack", price: 30 },
+];
+
+function addToCartTotal(item) {
+  console.log("item added:", item.name);
+  console.log("item price:", item.price);
+  cartTotal = cartTotal + item.price;
+  console.log("running total:", cartTotal);
+}
+
+function calculateCart() {
+  shoppingCart.forEach(addToCartTotal);
+  cartTotalText.textContent = cartTotal;
+}
+
+function checkDiscount() {
+  if (addToCartTotal > 100) {
+    cartTotal = cartTotal * 0.9;
+  }
+  cartTotalText.textContent = cartTotal;
+}
+
+console.log("Your cart total is:", cartTotal);
